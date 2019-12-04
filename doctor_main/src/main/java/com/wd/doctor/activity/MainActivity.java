f82a50ea13235844e574207dd089bf7b091ee60e
xutao0530 <1248525308@qq.com>
2019-12-04 08:25:04 +0800

    xt

---
+++ doctor_main/src/main/java/com/wd/doctor/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wd.doctor.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wd.doctor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
